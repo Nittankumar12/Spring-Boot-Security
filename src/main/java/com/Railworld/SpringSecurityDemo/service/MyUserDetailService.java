cabf8e79eb7a1ed505235fcac9ea8152f13c7d20
nittan <nitin01kumar14@gmail.com>
2024-03-11 14:55:34 +0530

    BCrypt

--- src/main/java/com/Railworld/SpringSecurityDemo/service/MyUserDetailService.java
+++ src/main/java/com/Railworld/SpringSecurityDemo/service/MyUserDetailService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MyUserDetailService implements UserDetailsService {
-
     @Autowired
     private UserRepo userRepo;
     @Override
